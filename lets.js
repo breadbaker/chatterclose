b398d3ee2a7f7c9847c7e76d5041a30646833d41
Dan Baker <dbaker@forio.com>
2016-04-24 16:44:29 -0400

    fix email

--- lets.js
+++ lets.js
@@ -11,7 +11,7 @@ var lex = LEX.create({
     // Note: this is the place to check your database to get the user associated with this domain
     cb(null, {
       domains: [hostname]
-    , email: 'CHANGE_ME' // user@example.com
+    , email: 'danielebaker@gmail.com' // user@example.com
     , agreeTos: true
     });
   }
