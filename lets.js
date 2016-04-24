6c5c276262964fd8f413cf80bae8fff6d4e099e7
Dan Baker <dbaker@forio.com>
2016-04-24 17:02:38 -0400

    new ptah

--- lets.js
+++ lets.js
@@ -6,7 +6,7 @@ Using .testing() will overwrite the debug flag with true */
 var LEX = require('letsencrypt-express');
 
 var lex = LEX.create({
-  configDir: '/etc/letsencrypt/live/www.chatterclose.com'
+  configDir: '/etc/letsencrypt/live/www.chatterclose.com/live/www.chatterclose.com'
 , approveRegistration: function (hostname, cb) { // leave `null` to disable automatic registration
     // Note: this is the place to check your database to get the user associated with this domain
     cb(null, {
